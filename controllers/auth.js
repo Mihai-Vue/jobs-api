aca69f70c69add39d559f91c4fc05cf912782b20
Mihai Boureanu <boureanumihai@yahoo.com>
2024-08-01 21:13:00 +0300

    implemented instance methods of the schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,15 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+const { BadRequest } = require("../errors");
+const jwt = require("jsonwebtoken");
+
 
 
 
 const register = async (req, res) => {
-// const { name, email, password } = req.body;
-//
-// const salt = await bcrypt.genSalt(10);
-// const hashedPassword = await bcrypt.hash(password, salt);
-//
-// const tempUser = { name, email, password:hashedPassword };
-
-
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(user);
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({user: {name: user.name}, token});
 };
 
 const login = async (req, res) => {
