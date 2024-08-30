ce35668d2aebeb54c52fffbd8023ad82b7b3b652
Mihai Boureanu <boureanumihai@yahoo.com>
2024-08-30 22:21:06 +0300

    code refactor

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//extra security packeges
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
@@ -15,8 +21,17 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requirestsper windowMs
+  }),
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
